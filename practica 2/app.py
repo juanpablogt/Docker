338c21f3217182508dbdaf810d8257183d83eaaa
JPablo <juapabgonzalez@utp.edu.co>
2026-02-09 09:35:49 -0500

    mno

--- practica 2/app.py
+++ practica 2/app.py
@@ -14,7 +14,8 @@ def getMyInfo():
             {"xUser": "aminespinoza"},
             {"linkedin": "amin-espinoza"},
             {"githubUser": "aminespinoza10"},
-            {"twitchUser": "aminespinoza10"}
+            {"twitchUser": "aminespinoza10"},
+            {"youtubeUser": "aminespinoza10"}
         ],
         "blog": "https://aminespinoza.com",
         "author": "Miranda Espinoza"
